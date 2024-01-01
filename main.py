6aff82eb66792bb22179b92edb01af147094fef8
Lookofsea <38491496+Lookofsea@users.noreply.github.com>
2024-01-01 15:21:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,10 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
+MD_HEAD = """**<p align="center">[Leo1226's Blog]()</p>**
 ====
 
-**<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
-[![](https://raw.githubusercontent.com/superleeyom/blog/main/img/IMG_216.JPEG)](https://blog.leeyom.top)
-
-## 联系方式
-- Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
-- Telegram：[@super_leeyom](https://t.me/super_leeyom)
-- Email：[leeyomwang@163.com](mailto:leeyomwang@163.com)
-- Blog：[https://blog.leeyom.top](https://blog.leeyom.top)
-- RSS：[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
-"""
+**<p align="center">随便写写</p>**
 
 BACKUP_DIR = "backup"
 ANCHOR_NUMBER = 5
